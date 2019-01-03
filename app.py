020beee1a6266bf9108e5346c4505979932627e5
muirurikin <muirurikin@gmail.com>
2019-01-03 23:00:32 +0300

    App Works

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-def main():
+from twilio.rest import TwilioRestClient
+from credentials import account_sid, auth_token, my_tel, my_twil
+
+client = TwilioRestClient(account_sid, auth_token)
+
+my_message = "Helllo From My App"
+
+message = client.messages.create(to=my_tel, from_= my_twil, body=my_message)
+
+print ('Message sent')
